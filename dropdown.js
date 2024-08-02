7c9bf8f00f05a9fcef26c8cec0d02401bbf55ba5
ShrinidhiM9 <sm6418@srmist.edu.in>
2024-08-02 19:15:22 +0530

    Create dropdown.js

---
+++ dropdown.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Dropdown = ({ options, selectedOptions, onSelect, onDeselect }) => {
+  return (
+    <select multiple>
+      {options.map((option) => (
+        <option key={option} value={option} selected={selectedOptions.includes(option)}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default Dropdown;
